265b3b955bfe82870fb1cae1ef4812ac9a1a187d
Taylor Shockley <tshockley736@gmail.com>
2024-04-15 17:24:54 -0400

    dockercompose file and env file updated

--- api/migrations/001_create_users.py
+++ api/migrations/001_create_users.py
@@ -4,8 +4,10 @@ steps = [
         """
         CREATE TABLE users (
             id SERIAL PRIMARY KEY NOT NULL,
-            username VARCHAR(100) NOT NULL UNIQUE,
-            password VARCHAR(256) NOT NULL
+            full_name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL UNIQUE,
+            password VARCHAR(256) NOT NULL,
+            linkedin_url VARCHAR(200) NOT NULL
         );
         """,
         # "Down" SQL statement
