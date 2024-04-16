c7c39aa63e265ccccd814f35e02c3abd39423974
Taylor Shockley <tshockley736@gmail.com>
2024-04-16 15:31:45 -0400

    pg-admin added to yaml services

--- api/migrations/001_create_users.py
+++ api/migrations/001_create_users.py
@@ -4,10 +4,8 @@ steps = [
         """
         CREATE TABLE users (
             id SERIAL PRIMARY KEY NOT NULL,
-            full_name VARCHAR(100) NOT NULL,
-            email VARCHAR(100) NOT NULL UNIQUE,
-            password VARCHAR(256) NOT NULL,
-            linkedin_url VARCHAR(200) NOT NULL
+            username VARCHAR(100) NOT NULL UNIQUE,
+            password VARCHAR(256) NOT NULL
         );
         """,
         # "Down" SQL statement
