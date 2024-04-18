a32ed2e8de49d31f73e067f7bb03283a9f8a2db3
Lauren Smith <mealerlauren@gmail.com>
2024-04-18 17:34:57 -0500

    added delete and get by ID

--- api/queries/job_queries.py
+++ api/queries/job_queries.py
@@ -44,6 +44,35 @@ class JobQueries:
             print(e)
             return {"message": "Could not get all jobs"}
 
+    def get_job_by_id(self, job_id: int):
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        SELECT id, image_url, position_title, company_name,
+                        job_description, creator_id
+                        FROM jobs
+                        WHERE id = %s
+                        """,
+                        [job_id],
+                    )
+                    record = db.fetchone()
+                    if record:
+                        return JobOut(
+                            id=record[0],
+                            image_url=record[1],
+                            position_title=record[2],
+                            company_name=record[3],
+                            job_description=record[4],
+                            creator_id=record[5],
+                        )
+                    else:
+                        return None
+        except Exception as e:
+            print(e)
+            return None
+
     def create_job(self, job: JobIn, creator_id: int) -> JobOut:
         try:
             with pool.connection() as conn:
@@ -62,8 +91,8 @@ class JobQueries:
                             job.position_title,
                             job.company_name,
                             job.job_description,
-                            creator_id
-                        ]
+                            creator_id,
+                        ],
                     )
                     id = result.fetchone()[0]
                     return self.job_in_to_out(id, job, creator_id)
@@ -72,8 +101,22 @@ class JobQueries:
             print(e)
             return {"message": "Creation did not work"}
 
+    def delete_job(self, job_id: int):
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        DELETE FROM jobs
+                        WHERE id = %s
+                        """,
+                        [job_id],
+                    )
+                    return db.rowcount > 0
+        except Exception as e:
+            print(e)
+            return False
+
     def job_in_to_out(self, id: int, job: JobIn, creator_id: int):
         old_data = job.dict()
         return JobOut(id=id, **old_data, creator_id=creator_id)
-
-
--- api/routers/job_router.py
+++ api/routers/job_router.py
@@ -26,18 +26,39 @@ router = APIRouter(tags=["Jobs"], prefix="/api/jobs")
 def get_all_jobs(
     repo: JobQueries = Depends(),
 ):
-    return {
-        "jobs": repo.get_all_jobs()
-    }
+    return {"jobs": repo.get_all_jobs()}
+
+
+@router.get("/{job_id}", response_model=JobOut)
+def get_job_by_id(
+    job_id: int,
+    job_queries: JobQueries = Depends(),
+):
+    job = job_queries.get_job_by_id(job_id)
+    if job:
+        return job
+    else:
+        raise HTTPException(status_code=404, detail="Job not found")
 
 
 @router.post("", response_model=JobOut)
 def create_job(
     job: JobIn,
     job_queries: JobQueries = Depends(),
-    user: UserResponse = Depends(try_get_jwt_user_data)
+    user: UserResponse = Depends(try_get_jwt_user_data),
 ):
     if user is None:
         raise HTTPException(status_code=401, detail="You must be logged in")
     job = job_queries.create_job(job=job, creator_id=user.id)
     return job
+
+
+@router.delete("/{job_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_job_by_id(
+    job_id: int,
+    job_queries: JobQueries = Depends(),
+):
+    if job_queries.delete_job(job_id):
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=404, detail="Job not found")
