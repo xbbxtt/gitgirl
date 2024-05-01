eb92fcc56eefb63c015b8485583870734bd752e7
alysongolden <alyson.golden714@gmail.com>
2024-05-01 14:36:07 -0500

    colors and buttons on home page done

--- ghi/src/components/Home.jsx
+++ ghi/src/components/Home.jsx
@@ -1,11 +1,15 @@
 // @ts-check
-import React from 'react'
+import { Link } from 'react-router-dom'
 import banner_3 from '/src/banner_3.jpg'
 
 function Home() {
     return (
         <div>
-            <div className="card mb-3 d-flex justify-content-around">
+            <div className="card mb-3 d-flex justify-content-around"
+                style={{
+                    border: 'none',
+                }}
+            >
                 <h1 className="card-title text-center">
                     <img
                         className="d-block user-select-none "
@@ -17,8 +21,17 @@ function Home() {
                 <div className="row">
                     <div className="col-md-8">
                         <div className="card-body">
-                            <h1 className="card-title">Mission Statement</h1>
-                            <div></div>
+                            <h1
+                                className="card-title"
+                                style={{
+                                    backgroundColor: 'transparent',
+                                    color: '#302939',
+                                    border: 'none',
+                                    marginBottom: '30px',
+                                }}
+                            >
+                                Mission Statement
+                            </h1>
                             <p className="card-text " style={{ width: '75%' }}>
                                 GitGirl exists to redefine the tech landscape
                                 into a realm where women and femme-presenting
@@ -33,7 +46,13 @@ function Home() {
                                 innovation, and career paths are coded with
                                 success.
                             </p>
-                            <p className="card-text" style={{ width: '75%' }}>
+                            <p
+                                className="card-text"
+                                style={{
+                                    width: '75%',
+                                    marginBottom: '30px',
+                                }}
+                            >
                                 We commit to creating an inclusive environment
                                 by connecting talents with opportunities. We aim
                                 to push the boundaries of what is possible,
@@ -41,22 +60,37 @@ function Home() {
                                 their goals, and advance in their careers and
                                 aspirations.
                             </p>
-                            <h2 className="card-subtitle text-muted">
+                            <h4
+                                className="card-subtitle text-muted"
+                                style={{ marginBottom: '20px' }}
+                            >
                                 Branch Out. Commit. Push Forward.
-                            </h2>
-                            <div className="d-flex m-3">
-                                <button
+                            </h4>
+                            <div className="d-flex m-4">
+                                <Link
+                                    to="/signin"
                                     type="button"
-                                    className="btn btn-secondary me-5"
+                                    className="btn me-4"
+                                    style={{
+                                        backgroundColor: '#302939',
+                                        color: 'white',
+                                        border: 'none',
+                                    }}
                                 >
                                     Sign In
-                                </button>
-                                <button
+                                </Link>
+                                <Link
+                                    to="/signup"
                                     type="button"
-                                    className="btn btn-outline-secondary"
+                                    className="btn"
+                                    style={{
+                                        backgroundColor: '#302939',
+                                        color: 'white',
+                                        border: 'none',
+                                    }}
                                 >
                                     Sign up
-                                </button>
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -71,33 +105,52 @@ function Home() {
                 </div>
             </div>
             <div className="row">
-                <div className="col">
-                    <div className="card text-white bg-primary mb-3 d-flex justify-content-around">
+                <div className="col-md-4" style={{ paddingRight: '0' }}>
+                    <div
+                        className="card mb-3 d-flex justify-content-around"
+                        style={{
+                            border: 'none',
+                            backgroundColor: 'transparent',
+                        }}
+                    >
                         <div
                             className="card-body"
                             style={{
                                 display: 'flex',
                                 alignItems: 'center',
                                 justifyContent: 'center',
+                                padding: '0',
                             }}
                         >
                             <img
-                                className="d-block user-select-none"
+                                className="d-block user-select-none img-fluid"
                                 src="https://images.unsplash.com/photo-1573165067541-4cd6d9837902?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                width="63.2%"
-                                height="auto"
+                                alt="Sample"
+                                style={{ borderRadius: '8px' }}
                             />
                         </div>
                     </div>
                 </div>
-                <div className="col">
+                <div className="col-md-8">
                     <div
-                        className="card text-white bg-primary mb-0"
-                        style={{ padding: '30px' }}
+                        className="card mb-0"
+                        style={{
+                            padding: '25px',
+                            color: '#302939',
+                            backgroundColor: '#F2C5C5',
+                        }}
                     >
                         <div className="card-body">
-                            <h1 className="card-title ">GitGirls</h1>
-                            <p className="card-text">
+                            <h1
+                                className="card-title"
+                                style={{ marginBottom: '35px' }}
+                            >
+                                GitGirls
+                            </h1>
+                            <p
+                                className="card-text"
+                                style={{ marginBottom: '25px' }}
+                            >
                                 Targeted Opportunities: GitGirl curates jobs,
                                 internships, and apprenticeships with employers
                                 who are committed to diversity and inclusion.
@@ -105,82 +158,143 @@ function Home() {
                                 listings that might not be as welcoming or
                                 supportive.
                             </p>
-                            <p>
+                            <p style={{ marginBottom: '25px' }}>
                                 Increased Visibility: By creating a profile on
                                 GitGirl, you enhance your visibility to
                                 inclusive employers actively looking to
                                 diversify their tech teams.
                             </p>
-                            <p>
+                            <p style={{ marginBottom: '50px' }}>
                                 Addressing the Gender Gap: GitGirl works towards
                                 bridging the gender gap in tech by creating a
                                 space where women and femme-presenting
                                 individuals are at the forefront and providing a
                                 more equitable access to opportunities.
                             </p>
-                            <p className="col-md-18 text-end">
-                                <button
+                            <div className="d-flex justify-content-end">
+                                <Link
+                                    to="/signin"
                                     type="button"
-                                    className="btn btn-secondary me-5"
+                                    className="btn me-4"
+                                    style={{
+                                        backgroundColor: '#302939',
+                                        color: 'white',
+                                        border: 'none',
+                                    }}
                                 >
                                     Sign In
-                                </button>
-                                <button
+                                </Link>
+                                <Link
+                                    to="/signup"
                                     type="button"
-                                    className="btn btn-secondary"
+                                    className="btn"
+                                    style={{
+                                        backgroundColor: '#302939',
+                                        color: 'white',
+                                        border: 'none',
+                                    }}
                                 >
                                     Join Network
-                                </button>
-                            </p>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="row">
-                <div className="col">
+                <div className="col-md-8" style={{ paddingRight: '0' }}>
                     <div
-                        className="card text-white bg-secondary mb-3 d-flex justify-content-around"
-                        style={{ padding: '50px' }}
+                        className="card mb-0"
+                        style={{
+                            padding: '25px',
+                            color: '#302939',
+                            backgroundColor: '#ffffff',
+                            border: 'none',
+                        }}
                     >
                         <div className="card-body">
-                            <h1 className="card-title">Employers</h1>
-                            <p className="card-text">
+                            <h1
+                                className="card-title"
+                                style={{ marginBottom: '35px' }}
+                            >
+                                Employers
+                            </h1>
+                            <p
+                                className="card-text"
+                                style={{ marginBottom: '25px' }}
+                            >
                                 Untapped Potential: GitGirl gives employers
                                 access to a pool of highly qualified women and
                                 femme-presenting individuals in tech who might
                                 be overlooked on traditional job boards.
                             </p>
-                            <p>
+                            <p style={{ marginBottom: '25px' }}>
                                 Diverse Talent: Employers can diversify their
                                 tech teams by connecting with candidates from
                                 underrepresented groups in tech.
                             </p>
-                            <p>
+                            <p style={{ marginBottom: '50px' }}>
                                 Positive Brand Image: Using GitGirl signals to
                                 potential candidates and the broader tech
                                 community that your company is actively
                                 committed to creating a diverse and inclusive
                                 workplace. This can boost employer branding and
-                                attract top talent who value inclusivity.
+                                attract top talent who value inclusiveness.
                             </p>
+                            <div className="d-flex">
+                                <Link
+                                    to="/signin"
+                                    type="button"
+                                    className="btn me-4"
+                                    style={{
+                                        backgroundColor: '#302939',
+                                        color: 'white',
+                                        border: 'none',
+                                    }}
+                                >
+                                    Sign In
+                                </Link>
+                                <Link
+                                    to="/signup"
+                                    type="button"
+                                    className="btn"
+                                    style={{
+                                        backgroundColor: '#302939',
+                                        color: 'white',
+                                        border: 'none',
+                                    }}
+                                >
+                                    Join Network
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="card text-white bg-secondary mb-0">
+                <div className="col-md-4" style={{ paddingRight: '0' }}>
+                    <div
+                        className="card mb-3 d-flex justify-content-around"
+                        style={{
+                            backgroundColor: 'transparent',
+                            border: 'none',
+                        }}
+                    >
                         <div
                             className="card-body"
                             style={{
                                 display: 'flex',
                                 alignItems: 'center',
                                 justifyContent: 'center',
+                                padding: '0',
+                                margin: '0',
                             }}
                         >
                             <img
-                                className="d-block user-select-none"
+                                className="d-block user-select-none img-fluid"
                                 src="https://images.unsplash.com/photo-1573165265437-f5e267bb3db6?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                width="60.9%"
-                                height="auto"
+                                alt="Sample"
+                                style={{
+                                    borderRadius: '8px',
+                                }}
                             />
                         </div>
                     </div>
